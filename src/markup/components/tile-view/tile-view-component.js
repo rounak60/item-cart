08e0fe76ebb26a6fd6131e06c501a261a28151a5
rounakMoonraft <49684813+rounakMoonraft@users.noreply.github.com>
2020-10-13 09:36:15 +0530

    discount changes

--- src/markup/components/tile-view/tile-view-component.js
+++ src/markup/components/tile-view/tile-view-component.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
 
-  // $.getJSON("/src/assets/items.json", function(res) {
-  //     console.log(res.items.name)
-  // })
-
 // JSON DATA
 var itemsList = {
     "items": [
@@ -83,6 +79,7 @@ var itemsList = {
   }
 
 
+// Getting Json data and showing in DOM
   var itemCountId = 0;
 $.each(itemsList.items, function(key, value) {
     $(".tile-view-container").append(`
@@ -95,8 +92,8 @@ $.each(itemsList.items, function(key, value) {
                 <div class="item-name">` + value.name + `</div>
                 <div class="item-details">
                     <div class="item-price">
-                        <span class="item-display-price">$` + value.price.display + `</span>
-                        <span class="item-actual-price">$` + value.price.actual + `</span>
+                        <span class="item-display-price" data-display="` + value.price.display +`">$` + value.price.display + `</span>
+                        <span class="item-actual-price" data-actual="` + value.price.actual + `">$` + value.price.actual + `</span>
                     </div>
                     <button class="add-to-cart-btn"> Add to Cart </button>
                 </div>
@@ -106,6 +103,8 @@ $.each(itemsList.items, function(key, value) {
     itemCountId++;
 })
 
+//Add to Cart Button functionalities
+var discValues = [];
 $(".add-to-cart-btn").on('click',function(){
     $(".header-toast-msg").empty();
     $(this).text("Added to cart");
@@ -130,35 +129,50 @@ $(".add-to-cart-btn").on('click',function(){
                                         </div>
                                         <div class=item-add-sub> 
                                             <input type="button" value="-" class="sub" >
-                                            <input type="text" size="10" value="1" class="totalCount">
+                                            <input type="text" size="10" value="1" class="totalCount" disabled>
                                             <input type="button" value="+" class="add" >
                                         </div>
                                         <div class="added-item-price">` + itemDisplayPrice + `</div>
                                         <div class="added-disc-price" style="display:none;">` + itemActualPrice + `</div>
                                     </div>`);
+
+    // Time out 3secs for toast message when added to cart
     setTimeout(function(){
         $('.header-toast-msg').hide();
     }, 3000);
 
+    // Item Count on cart
     var itemCount = $(".added-item").length;
     $(".total-items-added").text('Items' + '(' + itemCount + ')');
 
+    //Total price calc without discount
     var itemPriceAdd = $(".added-item-price").text().split('$');
     var totalItemPrice = itemPriceAdd.reduce(function(a, b){
         return a*1 + b*1;
     });
     $(".total-price").text('$' + totalItemPrice);
     
+    //Discount Price Calc
+    var displayPrice = $(this).siblings(".item-price").find(".item-display-price").attr("data-display");
+    var ActualPrice = $(this).siblings(".item-price").find(".item-actual-price").attr("data-actual");
+    var discountPrice = (displayPrice - ActualPrice);
+    
+    discValues.push(discountPrice);
+    var totalDiscPrice = discValues.reduce(function(a,b) {
+    return a*1 + b*1;
+    })
+    $(".disc-price").text('$' + totalDiscPrice);   
 
-
+    // Order Total Price Calc
+    $(".item-sub-total").text('$' + (totalItemPrice - totalDiscPrice));
 });
 
+// Remove items from Cart
 $(".added-items-list").on('click',".remove-item",function(){
     var cartItemId = $(this).closest(".added-item").attr("data-id");
     $("[data-id= " + cartItemId + " ]").find(".add-to-cart-btn").text("Add to cart");
     $("[data-id= " + cartItemId + " ]").find(".add-to-cart-btn").css("cursor","pointer");
     $("[data-id= " + cartItemId + " ]").find(".add-to-cart-btn").removeAttr("disabled");
-
 
     $(this).parents(".added-item").remove();
 
@@ -167,24 +181,36 @@ $(".added-items-list").on('click',".remove-item",function(){
 
     var itemCountRemove = $(".added-item").length;
     $(".total-items-added").text('Items' + '(' + itemCountRemove + ')');
+
+    // $(".item-sub-total").text(totalItemPrice - discountPrice);
 });
 
+// Increase same number of items in cart button
 $(".added-items-list").on('click',".add",function(){
+
     var  count = parseInt($(this).closest(".added-item").find(".totalCount").val());
     $(this).closest(".added-item").find(".totalCount").val(count + 1);
+
     var itemPriceAdd = $(this).closest(".added-item").find(".added-item-price").text().split('$')[1];
     var singleItemPriceAdd = itemPriceAdd/count;
     var newitemPrice = singleItemPriceAdd*(count + 1);
+
     $(this).closest(".added-item").find(".added-item-price").text('$'+ newitemPrice);
     $(".total-price").text('$' + (parseInt($(".total-price").text().split('$')[1])+(singleItemPriceAdd)));
+
+
 })
+
+//Decrease same number of items in cart button
 $(".added-items-list").on('click',".sub",function(){
     var  count = parseInt($(this).closest(".added-item").find(".totalCount").val());
     if (count > 1) {
         $(this).closest(".added-item").find(".totalCount").val(count - 1);
+
         var itemPriceSub = $(this).closest(".added-item").find(".added-item-price").text().split('$')[1];
         var singleItemPriceSub = itemPriceSub/count;
         var newitemPriceSub = singleItemPriceSub*(count - 1);
+
         $(this).closest(".added-item").find(".added-item-price").text('$'+ newitemPriceSub);
         $(".total-price").text('$' + (parseInt($(".total-price").text().split('$')[1])-(singleItemPriceSub)));
     }  
@@ -199,4 +225,9 @@ $(".added-items-list").on('click',".sub",function(){
 //       console.log(data);
 //     },
 //   });
+
+ // $.getJSON("/src/assets/items.json", function(res) {
+  //     console.log(res.items.name)
+  // })
+
 });
